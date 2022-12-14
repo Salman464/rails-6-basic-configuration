621de979a998b88cbb5157b4677032ed8f7062d0
Dev.Salman <sl0429905@gmail.com>
2022-12-14 18:38:37 +0500

    added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable,:confirmable
+  validates :first_name, :cnic, :contact, :dob, :address, presence: true
+  validates :cnic, :contact, numericality: { only_integer: true }
+  validate :must_be_valid_date
 
+  def must_be_valid_date
+    if dob >= Date.today
+      errors.add(:dob, "Invalid date of birth!(must be less than today)")
+    end
+  end
 end
