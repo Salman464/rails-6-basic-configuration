9fa53bd7e0621a7d4c3ea7894d24fba854a4ed37
Dev.Salman <sl0429905@gmail.com>
2022-12-18 21:54:22 +0500

    Added bootstrap to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable, :confirmable
          
-  validates :first_name, :cnic, :contact, :dob, :address, presence: true
-  validates :cnic, numericality: { only_integer: true }
-  validates :cnic, :email, uniqueness: true
-  validates :contact, phone: true
-  validate :must_be_valid_date
+  validates :first_name, :cnic, :contact, :dob, :address, :gender, presence: true, on: :update
+  validates :cnic, numericality: { only_integer: true }, on: :update
+  validates :cnic, length: { is: 13 }, on: :update
+  validates :cnic, :email, uniqueness: true, on: :update
+  validates :contact, phone: true, on: :update
+  validate :must_be_valid_date, on: :update
 
 
   def must_be_valid_date
+    if dob.nil?
+      errors.add(:dob, "Date of Birth was not provided!")
+      return false
+    end
     if dob >= Date.today
       errors.add(:dob, "Invalid date of birth!(must be less than today)")
       return false
